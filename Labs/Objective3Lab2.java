7d5a7334bc23482cece08f96bad55ed651f1b9b0
jbancr200 <jacoba1995@yahoo.com>
2023-05-02 16:58:30 -0600

    complete Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,8 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1 = Math.sqrt(3 * 3);
+		double side2 = Math.sqrt(2 * 2);
+		double hypotenuse = side1 + side2;
+		System.out.println(hypotenuse);
+	}
+}
