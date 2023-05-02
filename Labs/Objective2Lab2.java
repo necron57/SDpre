15fa2cc42d5acc8143dba211c5b5d00277d8c320
jbancr200 <jacoba1995@yahoo.com>
2023-05-02 15:10:15 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Name: Jacob Bancroft";
+		String age = "Age: 28";
+		String state = "State: Colorado";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+	}
+}
