e639168f39d7f5b511d5b4d64be53830e1a55e15
jbancr200 <jacoba1995@yahoo.com>
2023-05-08 14:34:37 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 0;
+    int sum = counter + counter;
+
+    while(counter <= 20) {
+    	sum = sum + counter;
+    	counter = counter + 1;
+    	
+    	
+    }
+    System.out.println(+ sum);
+  }
+}
