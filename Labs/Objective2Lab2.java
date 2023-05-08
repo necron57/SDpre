16c1b12601ce06d7cd031369b71bb59bd41aae47
jbancr200 <jacoba1995@yahoo.com>
2023-05-08 15:26:31 -0600

    complete objective2lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -1,10 +1,9 @@
 public class Objective2Lab2 {
 	public static void main(String[] args) {
-		String name = "Name: Jacob Bancroft";
-		String age = "Age: 28";
-		String state = "State: Colorado";
-		System.out.println(name);
-		System.out.println(age);
-		System.out.println(state);
+		String name = "Jacob Bancroft";
+		String age = "28";
+		String state = "Colorado";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
 	}
 }
