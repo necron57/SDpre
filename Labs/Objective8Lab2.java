9febc66cdb549c5a147838b29701b5add741dace
jbancr200 <jacoba1995@yahoo.com>
2023-05-09 12:43:47 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int counter, sum = 0;
+
+    for (counter = 1; counter <= 20; counter++){
+
+      sum = sum + counter;
+    }
+    System.out.println(sum);
+  }
+}
